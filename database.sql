810f201f6a0f7bcfe29fa297dca28c586b13060f
afazzdev <afaizi1999@gmail.com>
2020-06-10 14:52:29 +0700

    initial init and finish API

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE perntodo;
+
+CREATE TABLE todo
+(
+    todo_id BIGSERIAL NOT NULL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+); 
+
+-- \i "C:/Users/Pondok Programmer/Documents/faizi/faizi/javascript/fullstack/PERN todo list/server/database.sql"
